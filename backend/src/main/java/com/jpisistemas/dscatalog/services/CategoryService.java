a90fb309d0a2c7d17b6d34d9c13f7a307d89dba5
jeanfurquim <jean.furquim@jpisistemas.com.br>
2021-05-17 20:32:30 -0300

    Auditing Data

--- backend/src/main/java/com/jpisistemas/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jpisistemas/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jpisistemas.dscatalog.dto.CategoryDTO;
 import com.jpisistemas.dscatalog.entities.Category;
 import com.jpisistemas.dscatalog.repositories.CategoryRepository;
+import com.jpisistemas.dscatalog.services.exceptions.DatabaseException;
 import com.jpisistemas.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -54,8 +57,18 @@ public class CategoryService {
 			return new CategoryDTO(entity);
 
 		} catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Id not found" + id);
-			
+			throw new ResourceNotFoundException("Id not found " + id);
+
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
