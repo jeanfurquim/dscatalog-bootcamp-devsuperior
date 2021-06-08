0bee6face1a40e745c0859604e17586e40f588df
jeanfurquim <jean.furquim@jpisistemas.com.br>
2021-06-07 21:18:37 -0300

    Custom constraintValidator

--- backend/src/main/java/com/jpisistemas/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/jpisistemas/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.jpisistemas.dscatalog.dto;
 
+import com.jpisistemas.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
