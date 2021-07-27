df612d527c9fe0de50343c6bd246408d24a4c6a7
jeanfurquim <jean.furquim@jpisistemas.com.br>
2021-07-27 20:01:40 -0300

    Pagination Styling

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,6 +1,8 @@
+import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
+
 
 const Catalog = () => {
   const product: Product = {
@@ -52,6 +54,10 @@ const Catalog = () => {
             <ProductCard product={product} />
           </Link>
         </div>
+      </div>
+
+      <div className="row">
+        <Pagination />
       </div>
     </div>
   );
