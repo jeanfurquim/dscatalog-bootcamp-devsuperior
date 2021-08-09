0487fa93ddd41c6bc6f6c79ffb5b964a74951fba
jeanfurquim <jean.furquim@jpisistemas.com.br>
2021-08-08 21:45:44 -0300

    Login logout logic

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -2,11 +2,13 @@ import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
 
+
 import './styles.css';
 
 const Home = () => {
   return (
     <div className="home-container">
+
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
